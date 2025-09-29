72cff66e547d3aa183fe3322676122b23590db0a
shihab879 <159944693+shihab879@users.noreply.github.com>
2025-09-29 18:26:08 +0000

    Backend APIs

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); //allows us to accept json data in request body
+
+app.use("/api/products", productRoutes)
 
 app.listen(5000, () => {
   connectDB();
